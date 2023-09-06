5b41dbe73ed7452ec4bf77c88bb7bd7c6d0f1d17
Satyum <satyumspathania@gmail.com>
2023-09-07 03:56:36 +0530

    creating keypad of calculator.

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun OnDigit(view: View){
+        Toast.makeText(this,"Button clicked",Toast.LENGTH_SHORT).show()
     }
 }
