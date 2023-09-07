52ba6052b45e2293342cf4e8bb2fb9b2d37dd2b0
Satyum <satyumspathania@gmail.com>
2023-09-07 06:05:52 +0530

    using onClick and implementing clear function.

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,12 +3,18 @@ package com.example.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvInput : TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvInput = findViewById(R.id.tvInput)
     }
     fun OnDigit(view: View){
         Toast.makeText(this,"Button clicked",Toast.LENGTH_SHORT).show()
