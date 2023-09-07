75b75af8ea3262dfe73fba91152d37293de0264b
Satyum <satyumspathania@gmail.com>
2023-09-07 10:07:58 +0530

    Added AppIcon.

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -36,4 +36,22 @@ class MainActivity : AppCompatActivity() {
             lastDot = true
         }
     }
+
+    fun onOperator(view : View){
+        tvInput?.text?.let{
+            if(lastNumeric && !isOperatorAdded(it.toString())){
+                tvInput?.append((view as Button).text)
+                lastNumeric = false
+                lastDot = false
+            }
+        }
+    }
+
+    private fun isOperatorAdded(value : String) : Boolean{
+        return if(value.startsWith("-")) false
+        else{
+            value.contains("/") || value.contains("*") || value.contains("+") || value.contains("-")
+        }
+    }
+
 }
