31daa8009845535c6bb5d86fce979799aa2e6b8d
Satyum <satyumspathania@gmail.com>
2023-09-07 10:49:01 +0530

    Added EqualsTo functionality.

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -47,6 +47,26 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun onEqual(view : View){
+        if(lastNumeric){
+            var tvValue = tvInput?.text.toString()
+            var prefix = ""
+             try{
+                 if(tvValue.startsWith("-")){
+                     prefix="-"
+                     tvValue=tvValue.substring(1)
+                 }
+
+                 val splitValue = tvValue.split("-")
+                 var one = splitValue[0]
+                 var two = splitValue[1]
+                 tvInput?.text = (one.toDouble() - two.toDouble()).toString()
+             }catch(e:java.lang.ArithmeticException){
+                 e.printStackTrace()
+             }
+        }
+    }
+
     private fun isOperatorAdded(value : String) : Boolean{
         return if(value.startsWith("-")){
             false
